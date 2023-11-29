79aaefffde44431e2341a0972fe8e7c60b4b45b5
Eykan_ow <kallgrenerik@gmail.com>
2023-11-29 15:54:41 +0100

    added API query to get username from uuid

--- public/visual/navbar.php
+++ public/visual/navbar.php
@@ -1,5 +1,17 @@
 <div class="content">
 	<!-- navbar -->
+
+    <?php
+    $uuid = "71b8e290-9b39-4f4c-ae8f-2070a235d961";
+    $url = "https://playerdb.co/api/player/minecraft/".str_replace("-","",$uuid);
+
+    $playerjson = file_get_contents($url);
+    $metadata = json_decode($playerjson);
+
+    $name = $metadata->data->player->username;
+
+    echo $name;
+    ?>
 <ul>
 	<li><a href="index.php">Main</a> <br></li>
 	<li><a href="index.php?page=worlds">World</a></li>
